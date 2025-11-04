115a96b5d55cc36215b71d2fd9b9e797f237d909
GoCHarrbra <brad.harrison@ecn.forces.gc.ca>
2025-11-04 14:37:24 -0500

    v0.9.0

--- locals.tf
+++ locals.tf
@@ -1,13 +1,6 @@
 locals {
-  # If resource_prefix is provided (non-empty), use it; otherwise derive from name_prefix-env-location.
   derived_prefix = "${var.name_prefix}-${var.env}-${var.location}"
-
-  base_prefix = (trimspace(var.resource_prefix) != "")
-    ? var.resource_prefix
-    : local.derived_prefix
-
-  # Final KQL: replace token with the actual Standard Web Test name.
-  kql_final = replace(var.kql_query, "__WEB_TEST_NAME__", var.web_test_name)
-
-  tags_merged = var.tags
+  base_prefix    = trimspace(var.resource_prefix) != "" ? var.resource_prefix : local.derived_prefix
+  kql_final      = replace(var.kql_query, "__WEB_TEST_NAME__", var.web_test_name)
+  tags_merged    = var.tags
 }
